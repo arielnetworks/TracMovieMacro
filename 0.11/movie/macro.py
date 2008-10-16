ea649d0ebcde52a459b1dedec00e91466e63d68b
lcordier <lcordier@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-10-16 16:58:28 +0000

    Initial code import.
    git-svn-id: http://trac-hacks.org/svn/moviemacro@4518 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ 0.11/movie/macro.py
@@ -0,0 +1,244 @@
+""" Movie plugin for Trac.
+    
+    Embeds various online movies.
+"""
+from urlparse import urlparse, urlunparse
+from genshi.builder import tag
+from trac.core import *
+from trac.wiki.api import IWikiMacroProvider, parse_args
+from trac.web.chrome import ITemplateProvider, add_script
+from trac.wiki.macros import WikiMacroBase
+
+
+EMBED_COUNT = '_moviemacro_embed_count'
+
+def get_absolute_url(base, url):
+    """ Generate an absolute url from the url with the special schemes
+        {htdocs,chrome,ticket,wiki,source} simply return the url if given with
+        {http,https,ftp} schemes.
+        
+        Examples:
+            http://example.com/filename.ext
+                ie. http://www.google.com/logo.jpg
+            
+            chrome://site/filename.ext
+            htdocs://img/filename.ext
+                note: `chrome` is an alias for `htdocs`
+            
+            ticket://number/attachment.pdf
+                ie. ticket://123/specification.pdf
+            
+            wiki://WikiWord/attachment.jpg
+            
+            source://changeset/path/filename.ext
+                ie. source://1024/trunk/docs/README
+    """
+    def ujoin(*parts):
+        """ Remove double slashes.
+        """
+        parts = [part.strip('/') for part in parts]
+        return '/' + '/'.join(parts)
+    
+    if url.startswith(('ftp', 'http', 'https')):
+        return url
+    
+    if url.startswith('chrome:'):
+        url = url[7:]
+        return ujoin(base, 'chrome', url)
+    
+    if url.startswith('htdocs:'):
+        url = url[7:]
+        return ujoin(base, 'chrome', url)
+    
+    if url.startswith('source:'):
+        url = url[7:]
+        return ujoin(base, 'export', url)
+    
+    if url.startswith('ticket:'):
+        url = url[7:]
+        return ujoin(base, 'raw-attachment/ticket', url)
+    
+    if url.startswith('wiki:'):
+        url = url[5:]
+        return ujoin(base, 'raw-attachment/wiki', url)
+    
+    return url
+
+def string_keys(d):
+    """ Convert unicode keys into string keys, suiable for func(**d) use.
+    """
+    sdict = {}
+    for key, value in d.items():
+        sdict[str(key)] = value
+    
+    return sdict
+
+def xform_style(style):
+    """ Convert between a style-string and a style-dictionary.
+    """
+    if isinstance(style, dict):
+        result = '; '.join(['%s: %s' % (k, v) for k, v in style.items()])
+        if result:
+            result += ';'
+    else:
+        result = style.split(';')
+        while '' in result:
+            result.remove('')
+        
+        result = dict((s.strip() for s in i.split(':', 1)) for i in result)
+    
+    return result
+
+def xform_query(query):
+    """ Convert between a query-string and a query-dictionary.
+    """
+    if isinstance(query, dict):
+        result = '&'.join(['%s=%s' % (k, v) for k, v in query.items()])
+    else:
+        result = query.split('&')
+        while '' in result:
+            result.remove('')
+        
+        result = dict((s.strip() for s in i.split('=', 1)) for i in result)
+    
+    return result
+
+
+class MovieMacro(WikiMacroBase):
+    
+    implements(IWikiMacroProvider, ITemplateProvider)
+    
+    # IWikiMacroProvider methods
+    def expand_macro(self, formatter, name, content):
+        args, kwargs = parse_args(content, strict=True)
+        kwargs = string_keys(kwargs)
+        
+        if len(args) >= 1:
+            url = args[0]
+        elif len(args) == 0:
+            raise TracError('URL to the movie at least required. %s, %s' % (args, kwargs))
+        
+        embed_count = getattr(formatter, EMBED_COUNT, 0)
+        embed_count += 1
+        setattr(formatter, EMBED_COUNT, embed_count)
+        
+        url = get_absolute_url(formatter.href.base, url)
+        src = get_absolute_url(formatter.href.base, 'htdocs://movie/img/black.jpg')
+        
+        scheme, netloc, path, params, query, fragment = urlparse(url)
+        
+        try:
+            style_dict = xform_style(kwargs.get('style', ''))
+        except:
+            raise TracError('Double check the `style` argument.')
+        
+        style = {
+            'display': 'block',
+            'border': style_dict.get('border', 'none'),
+            'margin': style_dict.get('margin', '0 auto'),
+            'clear': 'both'
+        }
+        
+        if netloc == 'www.youtube.com':
+            query_dict = xform_query(query)
+            video = query_dict.get('v')
+            
+            url = urlunparse((scheme, netloc, '/v/%s' % video, '', '', ''))
+            
+            width = kwargs.pop('width', style_dict.get('width', '425px'))
+            height = kwargs.pop('height', style_dict.get('height', '344px'))
+            
+            style.update({
+                'width': width,
+                'height': height,
+            })
+            
+            return tag.object(tag.param(name='movie', value=url),
+                              tag.param(name='allowFullScreen', value='true'),
+                              tag.embed(src=url, type='application/x-shockwave-flash', allowfullscreen='true', width=width, height=height),
+                              style=xform_style(style))
+        
+        if netloc == 'video.google.com':
+            query_dict = xform_query(query)
+            query_dict['hl'] = 'en'
+            query_dict['fs'] = 'true'
+            
+            query = xform_query(query_dict)
+            
+            url = urlunparse((scheme, netloc, '/googleplayer.swf', '', query, ''))
+            
+            width = kwargs.pop('width', style_dict.get('width', '400px'))
+            height = kwargs.pop('height', style_dict.get('height', '326px'))
+            
+            style.update({
+                'width': width,
+                'height': height,
+            })
+            
+            return tag.embed(src=url,
+                             allowFullScreen='true',
+                             allowScriptAccess='always',
+                             type='application/x-shockwave-flash',
+                             style=xform_style(style))
+        
+        if netloc == 'www.metacafe.com':
+            parts = path.split('/')
+            try:
+                path = '/fplayer/%s/%s.swf' % (parts[2], parts[3])
+            except:
+                raise TracError("Non-standard URL, don't know how to process it, file a ticket please.")
+            
+            url = urlunparse((scheme, netloc, path, '', '', ''))
+            
+            width = kwargs.pop('width', style_dict.get('width', '400px'))
+            height = kwargs.pop('height', style_dict.get('height', '345px'))
+            
+            style.update({
+                'width': width,
+                'height': height,
+            })
+            
+            return tag.embed(src=url,
+                             wmode='transparent',
+                             pluginspage='http://www.macromedia.com/go/getflashplayer',
+                             type='application/x-shockwave-flash',
+                             style=xform_style(style))
+        
+        # Local movies.
+        tags = []
+        
+        if embed_count == 1:
+            add_script(formatter.req, 'movie/js/flashembed.min.js')
+            add_script(formatter.req, 'movie/js/flow.embed.js')
+            
+            script = '''
+                $(function() {
+                    $("a.flowplayer").flowembed("%s",  {initialScale:'scale'});		
+                });
+            ''' % get_absolute_url(formatter.href.base, 'htdocs://movie/swf/FlowPlayerDark.swf')
+            
+            tags.append(tag.script(script))
+        
+        width = kwargs.pop('width', style_dict.get('width', '320px'))
+        height = kwargs.pop('height', style_dict.get('height', '320px'))
+        
+        style.update({
+            'width': width,
+            'height': height,
+        })
+        
+        kwargs = {'style': xform_style(style)}
+        
+        tags.append(tag.a(tag.img(src=src, **kwargs), class_='flowplayer', href=url, **kwargs))
+        
+        return ''.join([str(i) for i in tags])
+    
+    # ITemplateProvider methods
+    def get_htdocs_dirs(self):
+        """ Makes the 'htdocs' folder inside the egg available.
+        """
+        from pkg_resources import resource_filename
+        return [('movie', resource_filename('movie', 'htdocs'))]
+    
+    def get_templates_dirs(self):
+        return []  # must return an iterable
---
+++ 0.11/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+description = '''\
+Embeds various online videos into wiki pages.
+Currently supports local files and YouTube, GoogleVideo and MetaCafe movies.
+
+This plugin makes use of the FlowPlayer [1] flash video player.
+If you are in a country where bandwidth comes at a premium considder
+downloading the content and placing it in you htdocs directory, then
+reference it as htdocs://site/filename.flv
+
+[1] http://flowplayer.org/
+[2] http://www.arrakis.es/~rggi3/youtube-dl/
+'''
+
+setup(
+    name = 'MovieMacro',
+    version = '0.1',
+    packages = ['movie'],
+    package_data = {'movie': ['htdocs/img/*.jpg',
+                              'htdocs/js/*.js',
+                              'htdocs/swf/*.swf']},
+    author = 'Louis Cordier',
+    author_email = 'lcordier@gmail.com',
+    description = description,
+    url = '',
+    license = 'BSD',
+    keywords = 'trac plugin movie macro',
+    classifiers = ['Framework :: Trac'],
+    entry_points = {'trac.plugins': ['movie.macro = movie.macro']}
+)
