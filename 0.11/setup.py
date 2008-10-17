1af55962e2ab4cef5a512f8e04a361cbff91022f
lcordier <lcordier@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-10-17 13:57:14 +0000

    Added macro's url.
    git-svn-id: http://trac-hacks.org/svn/moviemacro@4527 7322e99d-02ea-0310-aa39-e9a107903beb

--- 0.11/setup.py
+++ 0.11/setup.py
@@ -8,8 +8,8 @@ Currently supports local files and YouTube, GoogleVideo and MetaCafe movies.
 
 This plugin makes use of the FlowPlayer [1] flash video player.
 If you are in a country where bandwidth comes at a premium considder
-downloading the content and placing it in you htdocs directory, then
-reference it as htdocs://site/filename.flv
+downloading [2] the content and placing it in you htdocs directory,
+then reference it as htdocs://site/filename.flv
 
 [1] http://flowplayer.org/
 [2] http://www.arrakis.es/~rggi3/youtube-dl/
@@ -25,7 +25,7 @@ setup(
     author = 'Louis Cordier',
     author_email = 'lcordier@gmail.com',
     description = description,
-    url = '',
+    url = 'http://trac-hacks.org/wiki/MovieMacro/',
     license = 'BSD',
     keywords = 'trac plugin movie macro',
     classifiers = ['Framework :: Trac'],
